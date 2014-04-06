f3f99bc6cea7cf0234b816cc8ab4e49397887752
Monica Benitez <mony.23@hotmail.com>
2014-04-05 20:16:29 -0600

    Agregar una funcion - Icono

--- primer.py
+++ primer.py
@@ -12,16 +12,22 @@ class Ventana(QWidget):
     QWidget.__init__(self)
     self.setWindowTitle("Ventana")
     self.setGeometry(400, 200, 300, 250)
+    self.Imagen = 'gnulinux.jpg'
     
   def ponerImagen(self):
-    Imagen = QIcon('gnulinux.jpg')
+    Imagen = QIcon(self.Imagen)
     Label = QLabel('gnulinux', self)
     pixmapImagen = Imagen.pixmap(50,50)
     Label.setPixmap(pixmapImagen)
+  
+  def ponerIcono(self):
+    Icono = QIcon(self.Imagen)
+    self.setWindowIcon(Icono)
     
 if __name__=="__main__":
   aplicacion = QApplication (sys.argv)
   ventana = Ventana()
+  ventana.ponerIcono()
   ventana.ponerImagen()
   ventana.show()
   aplicacion.exec_()
