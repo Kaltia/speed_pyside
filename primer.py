d44b24086eac2fb486a82b5284cbeee6165d9440
Miguel Angel Gordian <os.aioria@gmail.com>
2014-04-12 19:41:57 -0500

    Seleccion de imagen desde comando y eleminacion de imagen por defecto

--- primer.py
+++ primer.py
@@ -5,30 +5,37 @@
 # git push origin rama{master}
 
 import sys
+import os
 from PySide.QtGui import QApplication, QWidget, QIcon, QLabel
-
+#Esta clase se Define la Ventana.
 class Ventana(QWidget):
   def __init__(self):
     QWidget.__init__(self)
     self.setWindowTitle("Ventana")
     self.setGeometry(400, 200, 300, 250)
-    self.Imagen = 'gnulinux.jpg'
     
-  def ponerImagen(self):
-    Imagen = QIcon(self.Imagen)
-    Label = QLabel('gnulinux', self)
+  def ponerImagen(self, img):
+    Imagen = QIcon(img)
+    Label = QLabel(os.path.basename(img), self)
     pixmapImagen = Imagen.pixmap(50,50)
     Label.setPixmap(pixmapImagen)
+    self.ponerIcono(img)
   
-  def ponerIcono(self):
-    Icono = QIcon(self.Imagen)
+  def ponerIcono(self, img):
+    Icono = QIcon(img)
     self.setWindowIcon(Icono)
     
 if __name__=="__main__":
+  if len(sys.argv) <= 1:
+    sys.exit(-1)
+    
+  if not os.path.exists(sys.argv[1]) or not os.path.isfile(sys.argv[1]):
+    sys.exit(-1)
+    
   aplicacion = QApplication (sys.argv)
   ventana = Ventana()
-  ventana.ponerIcono()
-  ventana.ponerImagen()
+  # ventana.ponerIcono()
+  ventana.ponerImagen(sys.argv[1])
   ventana.show()
   aplicacion.exec_()
   sys.exit()
